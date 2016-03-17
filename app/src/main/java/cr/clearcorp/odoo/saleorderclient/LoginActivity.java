56aab0d65cd7a96c5f57ff68af64ab389aa6fff2
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-03-17 12:53:26 -0600

    [FIX] - Fixed error validating empty password

--- app/src/main/java/cr/clearcorp/odoo/saleorderclient/LoginActivity.java
+++ app/src/main/java/cr/clearcorp/odoo/saleorderclient/LoginActivity.java
@@ -227,7 +227,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password)) {
+        if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
