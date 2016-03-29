280aa40d0a3b1da92fd190af0e6be1891a7fe15a
Glen Sojo <glen.sojo@clearcorp.co.cr>
2016-03-28 21:20:14 -0600

    [ADD] - Changed views to be Fragments

---
+++ app/src/main/java/cr/clearcorp/odoo/saleorderclient/LinesFragment.java
@@ -0,0 +1,24 @@
+package cr.clearcorp.odoo.saleorderclient;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LinesFragment extends Fragment {
+
+
+    public LinesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_lines, container, false);
+    }
+
+}
